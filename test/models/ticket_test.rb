a4ea2de4ac7b2290b5d92f4ade31008aa16ab2ad
Jeroen Versteeg <firm.grip0126@fastmail.com>
2025-12-08 13:00:48 +0100

    Ticket validates presence of title and description

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "title must be present" do
+    ticket = Ticket.new(description: "Test description")
+
+    refute ticket.valid?
+    assert_includes ticket.errors[:title], "can't be blank"
+  end
+
+  test "description must be present" do
+    ticket = Ticket.new(title: "Test title")
+
+    refute ticket.valid?
+    assert_includes ticket.errors[:description], "can't be blank"
+  end
 end
